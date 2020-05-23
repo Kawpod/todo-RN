36186a0d95bacf7eb7b0d213d7656def5dc0156a
Evghenii Covalenco <halif6243@gmail.com>
2020-05-23 23:36:09 +0300

    first

---
+++ App.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, FlatList, Alert } from 'react-native'
+import { Navbar } from './src/components/Navbar'
+import { MainScreen } from './src/screens/MainScreen'
+import { TodoScreen } from './src/screens/TodoScreen'
+
+export default function App() {
+  const [todoId, setTodoId] = useState('2')
+  const [todos, setTodos] = useState([
+    { id: '1', title: 'Выучить React Native' },
+    { id: '2', title: 'Написать приложение' }
+  ])
+
+  const addTodo = title => {
+    setTodos(prev => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title
+      }
+    ])
+  }
+
+  const removeTodo = id => {
+    const todo = todos.find(t => t.id === id)
+    Alert.alert(
+      'Удаление элемента',
+      `Вы уверены, что хотите удалить "${todo.title}"?`,
+      [
+        {
+          text: 'Отмена',
+          style: 'cancel'
+        },
+        {
+          text: 'Удалить',
+          style: 'destructive',
+          onPress: () => {
+            setTodoId(null)
+            setTodos(prev => prev.filter(todo => todo.id !== id))
+          }
+        }
+      ],
+      { cancelable: false }
+    )
+  }
+
+  let content = (
+    <MainScreen
+      todos={todos}
+      addTodo={addTodo}
+      removeTodo={removeTodo}
+      openTodo={setTodoId}
+    />
+  )
+
+  if (todoId) {
+    const selectedTodo = todos.find(todo => todo.id === todoId)
+    content = (
+      <TodoScreen
+        onRemove={removeTodo}
+        goBack={() => setTodoId(null)}
+        todo={selectedTodo}
+      />
+    )
+  }
+
+  return (
+    <View>
+      <Navbar title='Todo App!' />
+      <View style={styles.container}>{content}</View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 30,
+    paddingVertical: 20
+  }
+})
